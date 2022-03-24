3dce0d5f036e599a6c75c6685af0735e75ea1943
Antti <anttituomola8@gmail.com>
2022-03-24 15:35:56 +0200

    initial redux data

--- src/components/Metadata.js
+++ src/components/Metadata.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import * as metaActions from "../reducers/metadata"
 
 const Metadata = () => {
+  const wholeState = useSelector(state => state)
+  const dispatch = useDispatch()
 
   return (
     <div className='metadata'>
@@ -8,31 +12,38 @@ const Metadata = () => {
         Plan name
       </div>
       <div className="metadataElement right">
-        <input id="menuNameInput" type="text" />
+        <input id="menuNameInput" type="text" onChange={(event) => dispatch(metaActions.menuName(event.target.value))}/>
       </div>
       <div className="metadataElement">
         Number of diners
       </div>
       <div className="metadataElement right">
-        <button className="metaMinus">-</button><span className="metaNumber">2</span><button>+</button>
+        <button className="metaMinus" onClick={() => dispatch(metaActions.decrementDiners())}>-</button>
+        <span className="metaNumber">{wholeState.metadata.diners}</span>
+        <button onClick={(event) => dispatch(metaActions.incrementDiners(event.target.value))}>+</button>
       </div>
       <div className="metadataElement">
         Number of days
       </div>
       <div className="metadataElement right">
-        <button className="metaMinus">-</button><span className="metaNumber">2</span><button>+</button>
+        <button className="metaMinus" onClick={() => dispatch(metaActions.decrementDays())}>-</button>
+        <span className="metaNumber">{wholeState.metadata.days}</span>
+        <button onClick={() => dispatch(metaActions.incrementDays())}>+</button>
       </div>
       <div className="metadataElement">
         Portions per day
       </div>
       <div className="metadataElement right">
-        <button className="metaMinus">-</button><span className="metaNumber">2</span><button>+</button>
+        <button className="metaMinus" onClick={() => dispatch(metaActions.decrementPortions())}>-</button>
+        <span className="metaNumber">{wholeState.metadata.portionsPerDay}</span>
+        <button onClick={() => dispatch(metaActions.incrementPortions())}>+</button>
       </div>
       <div className="metadataElement">
         Allergies
       </div>
       <div className="metadataElement right">
-        <span>Nuts</span><span>Kiwi</span>
+        <input type="text" onChange={(event) => dispatch(metaActions.addAllergy(event.target.value))}></input>
+        <div>{wholeState.metadata.allergies}</div>
       </div>
     </div>
   )
--- src/reducers/metadata.js
+++ src/reducers/metadata.js
@@ -4,19 +4,41 @@ const initialState = {
     menuName: "",
     diners: 0,
     days: 0,
-    portionsPerDay: 0
+    portionsPerDay: 0,
+    allergies: []
 }
 
 export const metadataSlice = createSlice({
     name: "metadata",
     initialState: initialState,
     reducers: {
-        updateMetadata: (state, action) => {
-            state.value = action.payload
+        menuName: (state, action) => {
+            state.menuName = action.payload
+        },
+        incrementDiners: (state) => {
+            state.diners += 1
+        },
+        decrementDiners: (state) => {
+            state.diners -= 1
+        },
+        incrementDays: (state) => {
+            state.days += 1
+        },
+        decrementDays: (state) => {
+            state.days -= 1
+        },
+        incrementPortions: (state) => {
+            state.portionsPerDay += 1
+        },
+        decrementPortions: (state) => {
+            state.portionsPerDay -= 1
+        },
+        addAllergy: (state, action) => {
+            state.allergies = action.payload
         }
     }
 })
 
-export const { updateMetadata } = metadataSlice.actions
+export const { menuName, incrementDiners, decrementDiners, incrementDays, decrementDays, incrementPortions, decrementPortions, addAllergy } = metadataSlice.actions
 
 export default metadataSlice.reducer
