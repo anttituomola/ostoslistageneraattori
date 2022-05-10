dd118299e58fe84252ed216b40147db0a0891525
Antti <anttituomola8@gmail.com>
2022-05-10 10:44:25 +0300

    removed old comments

--- src/components/Ingredients.js
+++ src/components/Ingredients.js
@@ -24,6 +24,7 @@ export const Ingredients = () => {
     recipeRow => flatRecipeRowIds.includes(recipeRow.id)
   )
 
+  // Display ingredients and amounts
   ingredients = recipeRowsToDisplay.map(recipeRow => {
     return <div key={recipeRow.id}>{recipeRow.fields.Name}, {recipeRow.fields.AmountPerPerson * diners} {recipeRow.fields.Unit}</div>
     })
@@ -32,37 +33,3 @@ export const Ingredients = () => {
     <div>{ingredients}</div>
   )
 }
-
-    /*   if (recipes.length !== 0) {
-        // Get every recipeRow id mentioned in the recipes
-        const recipeRowsInRecipes = recipes.map(recipe => {
-          return recipe.fields.RecipeRows
-        })
-    
-        // Flatten the array of arrays
-        const recipeRowsInRecipesFlat = recipeRowsInRecipes.flat()
-    
-        // Find matching id's in recipeRowData
-        const matchingRecipeRows = recipeRowData.filter(recipeRow => {
-          return recipeRowsInRecipesFlat.includes(recipeRow.id)
-        })
-    
-        // Get ingredient IDs from matchingRecipeRows
-        const ingredientIds = matchingRecipeRows.map(recipeRow => {
-          return recipeRow.fields.Ingredient
-        })
-    
-        // Flatten the array of arrays
-        const ingredientIdsFlat = ingredientIds.flat()
-    
-        // Find matching ingredient IDs in ingredientData
-        const matchingIngredients = ingredientsData.filter(ingredient => {
-          return ingredientIdsFlat.includes(ingredient.id)
-        })
-        console.log(matchingIngredients)
-    
-        // Render ingredients name
-        ingredients = matchingIngredients.map(recipeRow => {
-          return <p key={recipeRow.id}>{recipeRow.fields.Name}, {recipeRow.fields.AmountPerPerson} {recipeRow.fields.Unit}</p>
-        })
-      } */
